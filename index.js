98a47332b7fcf614a155f70bfbbb02be0bce1f59
Rumman Ahmed <ahmed.rumman@gmail.com>
2019-03-26 13:05:02 +1100

    automated fullstak cli added with new config

--- index.js
+++ index.js
@@ -1,77 +1,34 @@
-const program = require('commander')
-const { prompt } = require('inquirer')
-const {newContactPrompts} = require('./prompts')
-const { getContacts, saveContacts } = require('./utils')
-
-const fs = require('fs')
-
-const templateData = fs.readFileSync('./_template/model.js', 'utf8')
+const express = require('express')
+const fs = require('fs');
 
 
-async function readTemplate () {
-  try {
-    const data = await fs.readFile('./_template/model.js', (err,val) => {
-      if (err) throw err
-      let a = val;
-      return a;
-    });
-  }
-  catch (err) { console.error( err ) }
-};
+const app = express();
+const port = 9000
 
-const templateDataAsync = readTemplate()
+const pageSync = fs.readFileSync('page.html', 'utf8');
 
-function test() {
-  console.log('file added');
-}
-
-program
-  .version('0.0.1')
-  .description('Address book CLI program')
-
-program
-  .command('new')
-  .alias('n')
-  .description('add a new contact')
-  .action(() => {
-    prompt(newContactPrompts)
-      .then(({ model}) => {
-        fs.mkdir(model, 0o776, (err) => {
-          try {
-            fs.writeFileSync(`${model}/${model}.js`,`(${test.toString()}())`);
-            fs.writeFileSync(`${model}/${model}.routes.js`,`${templateData}`)
-            fs.writeFileSync(`${model}/${model}.controler.js`,`${test.toString()}`)
-            fs.writeFileSync(`${model}/${model}.dummyData.js`,`${test.toString()}`)
-          } catch (e) {
-              console.log("File creation not possible",e)
-          }
+async function readFileData() {
+    try {
+        const data = await fs.readFile('page.js', (err, val) => {
+            if (err) throw err;
+            let file = val;
+            return file;
         })
-       
-        // const key = model + ' ' + route
-        // const contacts = getContacts()
-        // contacts[key] = {model, route, controller}
-        // saveContacts(contacts)
-      })
-  })
+    } catch (err) {
+        console.error(err)
+    }
+}
+// const a = readFileData();
+// console.log(a)
 
-program
-  .command('list')
-  .alias('l')
-  .description('list all contacts')
-  .action(() => {
-    const contacts = getContacts()
-    prompt([
-      {
-        type: 'list',
-        name: 'selected',
-        message: 'Select a contact',
-        choices: Object.keys(contacts)
-      }
-    ])
-      .then(({selected}) => {
-        const contact = contacts[selected]
-        console.log(JSON.stringify(contact, null, 2))
-      })
-  })
+// console.log('pageSync',pageSync)
 
-program.parse(process.argv)
+app.get('/', (req, res) => {
+    res.send(pageSync)
+})
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server is running on ",port)
+    }
+})
